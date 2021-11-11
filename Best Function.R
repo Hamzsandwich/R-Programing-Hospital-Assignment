7100c090e6b6a6e6f88132c1fda6c6e3000c1641
Hamzsandwich <doyle860@gmail.com>
2021-11-10 20:34:56 -0800

    initail commit

---
+++ Best Function.R
@@ -0,0 +1,16 @@
+best <- function(state, admit_reas) {
+    ## read in the data from CSV to Data frame
+    
+    ## Clean the data by keeping only needed col
+    ## 30 day outcome for 
+        ## heart attach
+        ## heart failure
+        ## pneumaia
+    ## removing NAs
+    
+    ## Create a df for the called state
+    
+    ## sort by admit_reason
+    
+    ## call best hospital name (lowest 30 mort) for that 
+}
